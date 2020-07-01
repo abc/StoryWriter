bdc05bfdb54f1a2d9ee7007cc6b9bec6fa9dbbb3
Alastair Campbell <alastair.campbell@runbox.no>
2020-07-01 11:58:22 +0100

    Add some XMLDoc documentation.

--- StoryWriter/Controllers/HomeController.cs
+++ StoryWriter/Controllers/HomeController.cs
@@ -6,8 +6,18 @@ using System.Web.Mvc;
 
 namespace StoryWriter.Controllers
 {
+    /// <summary>
+    /// This is a simple one-page controller which simply forwards the user to our index view.
+    /// </summary>
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// The index page for the application - just display the homepage.
+        /// </summary>
+        /// <returns>
+        /// The index view result.
+        /// </returns>
         public ActionResult Index()
         {
             return View();
--- StoryWriter/Controllers/StoryController.cs
+++ StoryWriter/Controllers/StoryController.cs
@@ -9,36 +9,61 @@ using Newtonsoft.Json;
 
 namespace StoryWriter.Controllers
 {
+    /// <summary>
+    /// The main controller for the project, the story controller handles a lot of the application logic.
+    /// </summary>
     public class StoryController : Controller
     {
-        // 
+        /// <summary>
+        /// The entry-point for the application. If the user is logged in, this will
+        /// forward them to their active room or the "Join/Host Room" page.
+        /// Otherwise, it asks them to sign in.
+        /// </summary>
+        /// <returns>
+        /// The Index view, which is a login page, or a redirect to either the "Join/Host Room"
+        /// or their current room if they're in a room.
+        /// </returns>
         public ActionResult Index()
         {
             // Check to see if this user is already logged in!
-
             var writer = SessionService.GetWriter(Session[SessionVariables.UserId]);
 
             if (writer == null)
             {
+                // Get the user to login
                 return View();
             }
 
+            // Does the user already have a room?
             var room = SessionService.GetRoom(Session[SessionVariables.RoomCode]);
             
             if (room == null)
             {
+                // If not, let them join or create a room.
                 return RedirectToAction("Start");
             }
 
+            // If they're in a room, redirect them to it.
             return RedirectToAction("Room", new { Id = room.Code });
         }
 
+        /// <summary>
+        /// Clear the user's session, allowing them to start fresh.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ForgetMe()
         {
             Session.Clear();
             return RedirectToAction("Index");
         }
 
+        /// <summary>
+        /// Get the current story for a given room and render it into a partial view.
+        /// </summary>
+        /// <param name="Id">Room code to get the story for.</param>
+        /// <returns>
+        /// A partial view displaying the room's story.
+        /// </returns>
         public ActionResult StoryText (string Id)
         {
             // ID = Room code.
@@ -53,6 +78,7 @@ namespace StoryWriter.Controllers
                 return null;
             }
 
+            // Return a partial view containing the room's story so far.
             return PartialView(room.Story);
         }
 
